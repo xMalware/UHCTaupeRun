41dcd600c626b38c090f96cded9606a86eabbc74
xMalware <xmalware2@gmail.com>
2016-12-09 13:04:19 +0100

    fix

--- src/fr/badblock/speeduhc/commands/GameCommand.java
+++ src/fr/badblock/speeduhc/commands/GameCommand.java
@@ -65,7 +65,7 @@ public class GameCommand extends AbstractCommand {
 				plug.setMaxPlayers(GameAPI.getAPI().getTeams().size() * perTeam);
 				try {
 					BukkitUtils.setMaxPlayers(GameAPI.getAPI().getTeams().size() * perTeam);
-				} catch (ReflectiveOperationException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				
