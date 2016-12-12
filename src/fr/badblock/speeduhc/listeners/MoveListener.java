98082eae1bbc38bdc83d0e9721f223a62b46496a
Asus <Asus@DESKTOP-LLJFP22>
2016-12-12 12:18:50 +0100

    Tourner dans le vide vide

--- src/fr/badblock/speeduhc/listeners/MoveListener.java
+++ src/fr/badblock/speeduhc/listeners/MoveListener.java
@@ -1,11 +1,15 @@
 package fr.badblock.speeduhc.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import fr.badblock.gameapi.BadListener;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.players.BadblockPlayer.BadblockMode;
 import fr.badblock.speeduhc.PluginUHC;
 
 public class MoveListener extends BadListener {
@@ -27,6 +31,15 @@ public class MoveListener extends BadListener {
 			
 			if(vehicle != null)
 				vehicle.setPassenger(e.getPlayer());
+		}else if(e.getTo().getY() <= 0.0d && inGame()){
+			BadblockPlayer player = (BadblockPlayer) e.getPlayer();
+			if (player.getBadblockMode().equals(BadblockMode.PLAYER)) {
+				@SuppressWarnings("deprecation")
+				EntityDamageEvent event = new EntityDamageEvent(player, EntityDamageEvent.DamageCause.VOID, player.getHealth());
+				player.setLastDamageCause(event);
+				Bukkit.getServer().getPluginManager().callEvent(event);
+				player.setHealth(0.0D);
+			}
 		}
 	}
 }
